e25cdaab5e9e938f06b715d47b9790f754de18f2
abhishek <abhishek-2863@abhishek-2863.csez.zohocorpin.com>
2017-04-02 22:32:03 +0530

    starter/skeleton version

---
+++ src/main/java/com/example/giffy/GiffyDiffyApplication.java
@@ -0,0 +1,12 @@
+package com.example.giffy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiffyDiffyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiffyDiffyApplication.class, args);
+	}
+}
